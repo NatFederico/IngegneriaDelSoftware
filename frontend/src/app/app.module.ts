6b50531e0018e392d1f379c7ed84806e945cbb52
sarasorre <sara.sorrentino@studenti.unitn.it>
2023-06-02 12:15:34 +0200

    🦾 Basic Magic Link Login + Dashboard Pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,11 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
 import { DepartmentComponent } from './pages/department/department.component';
 import { DocsComponent } from './pages/docs/docs.component';
+import { AuthComponent } from './auth/auth.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { AccountComponent } from './account/account.component';
+import { TeamComponent } from './pages/team/team.component';
+import { BoardComponent } from './pages/board/board.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +21,17 @@ import { DocsComponent } from './pages/docs/docs.component';
     DashboardComponent,
     GalleryComponent,
     DepartmentComponent,
-    DocsComponent
+    DocsComponent,
+    AuthComponent,
+    AccountComponent,
+    TeamComponent,
+    BoardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://vuoensqmahxqjlhwnqrr.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1b2Vuc3FtYWh4cWpsaHducXJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUzNDI2NjksImV4cCI6MjAwMDkxODY2OX0.pk0Rz8cHTYelF2Wg5kXwhA87mrzlZBNsZMHiIlbUe74',
 };
 
 /*
