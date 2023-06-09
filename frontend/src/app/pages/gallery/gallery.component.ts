7221bd91bedf1dc1de8b911d39d500b4700065a3
Mirko <mirko.bellini@studenti.unitn.it>
2023-06-09 23:01:57 +0200

    🧼 Aggiornamento gallery e stats

--- frontend/src/app/pages/gallery/gallery.component.ts
+++ frontend/src/app/pages/gallery/gallery.component.ts
@@ -16,4 +16,45 @@ export class GalleryComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  changeNameGallery() {
+    let changeNameBTN = document.getElementById("changeNameBTN");
+    let changeNameText = document.getElementById("changeNameText");
+    let changeNameSubmit = document.getElementById("changeNameSubmit");
+
+    changeNameText.classList.remove('hidden');
+    changeNameSubmit.classList.remove('hidden');
+    changeNameBTN.classList.add('hidden');
+
+  }
+
+  setNewName() {
+    let titleGallery = document.getElementById("titleGallery");
+    let changeNameBTN = document.getElementById("changeNameBTN");
+    let changeNameText = document.getElementById("changeNameText") as HTMLInputElement;
+    let changeNameSubmit = document.getElementById("changeNameSubmit");
+
+    changeNameText.classList.add('hidden');
+    changeNameSubmit.classList.add('hidden');
+    changeNameBTN.classList.remove('hidden');
+
+    if(changeNameText.value !== ''){
+      titleGallery.innerText = changeNameText.value;
+    }
+  }
+
+  displayForm() {
+    let nuovaImg = document.getElementById("nuovaImg");
+    let displayFormBTN = document.getElementById("displayFormBTN");
+
+    nuovaImg.classList.remove('hidden');
+    displayFormBTN.classList.add('hidden');
+  }
+
+  hideForm() {
+    let nuovaImg = document.getElementById("nuovaImg");
+    let displayFormBTN = document.getElementById("displayFormBTN");
+
+    nuovaImg.classList.add('hidden');
+    displayFormBTN.classList.remove('hidden');
+  }
 }
